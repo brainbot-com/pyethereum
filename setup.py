316893e5ac052d08e35c29ae005432800101571a
Ralf Schmitt <ralf@systemexit.de>
2014-03-28 22:36:10 +0100

    remove trie console_script
    given the fact that it didn't work at all and the limited usefulness of
    pyethereum.trie as a script, remove it.
    
    those who like to run it anyway, may type "python -m pyethereum.trie
    ..." instead.

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 
 from setuptools import setup, find_packages
 
-console_scripts = ['trie=pyethereum.trie:main',
-                   'eth=pyethereum.eth:main']
+console_scripts = ['eth=pyethereum.eth:main']
 
 setup(name="pyethereum",
       packages=find_packages("."),
-      install_requires=['leveldb', 'pybitcointools', 'pysha3'],
-      entry_points=dict(console_scripts=console_scripts))
+      install_requires=['leveldb', 'pybitcointools', 'pysha3'])
