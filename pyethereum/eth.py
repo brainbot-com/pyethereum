d5e9bb6dac687290dc007fe806836385020a7e6a
Heiko Heiko <heiko@heiko.org>
2014-03-31 19:10:07 +0200

    interactive mode via ipython

--- pyethereum/eth.py
+++ pyethereum/eth.py
@@ -29,11 +29,12 @@ def create_config():
     config.set('misc', 'verbosity', '1')
     config.set('misc', 'config_file', None)
     config.set('misc', 'logging', None)
+    config.set('misc', 'interactive', "False")
 
     config.add_section('wallet')
-
     # NODE_ID == pubkey, needed in order to work with Ethereum(++)
-    config.set('wallet', 'pub_key', 'J\x02U\xfaFs\xfa\xa3\x0f\xc5\xab\xfd<U\x0b\xfd\xbc\r<\x97=5\xf7&F:\xf8\x1cT\xa02\x81\xcf\xff"\xc5\xf5\x96[8\xacc\x01R\x98wW\xa3\x17\x82G\x85I\xc3o|\x84\xcbD6\xbay\xd6\xd9')
+    config.set('wallet', 'pub_key',
+               'J\x02U\xfaFs\xfa\xa3\x0f\xc5\xab\xfd<U\x0b\xfd\xbc\r<\x97=5\xf7&F:\xf8\x1cT\xa02\x81\xcf\xff"\xc5\xf5\x96[8\xacc\x01R\x98wW\xa3\x17\x82G\x85I\xc3o|\x84\xcbD6\xbay\xd6\xd9')
 
     usage = "usage: %prog [options]"
     parser = OptionParser(usage=usage,  version="%prog 0.1a")
@@ -66,7 +67,11 @@ def create_config():
                       help="<number>  Attempt to connect to given number of peers (default: 5)")
     parser.add_option("-C", "--config",
                       dest="config_file",
-                      help="read coniguration")
+                      help="read coniguration"),
+    parser.add_option("-i", "--interactive",
+                      action="store_true",
+                      dest="interactive",
+                      help="start ipython shell")
 
     (options, args) = parser.parse_args()
 
@@ -78,6 +83,8 @@ def create_config():
     for attr in ('verbosity', 'config_file'):
         config.set(
             'misc', attr, getattr(options, attr) or config.get('misc', attr))
+    config.set('misc', 'interactive', (
+        options.interactive and "True") or config.get('misc', 'interactive'))
 
     if len(args) != 0:
         parser.error("wrong number of arguments")
@@ -86,45 +93,48 @@ def create_config():
     if config.get('misc', 'config_file'):
         config.read(config.get('misc', 'config_file'))
 
-    #configure logging
+    # configure logging
     loggerlevels = getattr(options, 'logging') or ''
-    configure_logging(loggerlevels, verbosity=config.getint('misc', 'verbosity'))
+    configure_logging(
+        loggerlevels, verbosity=config.getint('misc', 'verbosity'))
 
     return config
 
 
 def configure_logging(loggerlevels, verbosity=1):
     logconfig = dict(version=1,
-                    disable_existing_loggers=False,
-                    formatters=dict(
-                        debug=dict(
-                            format='[%(asctime)s] %(name)s %(levelname)s %(threadName)s: %(message)s'
-                        ),
-                        minimal=dict(
-                            format='%(message)s'
-                        ),
-                    ),
-                    handlers=dict(
-                        default={
-                            'level': 'INFO',
-                            'class': 'logging.StreamHandler',
-                            'formatter': 'minimal'
-                        },
-                        verbose={
-                            'level': 'DEBUG',
-                            'class': 'logging.StreamHandler',
-                            'formatter': 'debug'
-                        },
-                    ),
-                    loggers=dict()
-                    )
+                     disable_existing_loggers=False,
+                     formatters=dict(
+                         debug=dict(
+                             format='[%(asctime)s] %(name)s %(levelname)s %(threadName)s: %(message)s'
+                         ),
+                         minimal=dict(
+                             format='%(message)s'
+                         ),
+                     ),
+                     handlers=dict(
+                         default={
+                             'level': 'INFO',
+                             'class': 'logging.StreamHandler',
+                             'formatter': 'minimal'
+                         },
+                         verbose={
+                             'level': 'DEBUG',
+                             'class': 'logging.StreamHandler',
+                             'formatter': 'debug'
+                         },
+                     ),
+                     loggers=dict()
+                     )
 
     for loggerlevel in filter(lambda _: ':' in _, loggerlevels.split(',')):
         name, level = loggerlevel.split(':')
-        logconfig['loggers'][name] = dict(handlers=['verbose'], level=level, propagate=False)
+        logconfig['loggers'][name] = dict(
+            handlers=['verbose'], level=level, propagate=False)
 
     if len(logconfig['loggers']) == 0:
-        logconfig['loggers'][''] = dict(handlers=['default'], level={0: 'ERROR', 1: 'WARNING', 2: 'INFO', 3: 'DEBUG'}.get(verbosity), propagate=True)
+        logconfig['loggers'][''] = dict(handlers=['default'], level={
+                                        0: 'ERROR', 1: 'WARNING', 2: 'INFO', 3: 'DEBUG'}.get(verbosity), propagate=True)
 
     logging.config.dictConfig(logconfig)
     logging.debug("logging set up like that: {0}".format(logconfig))
@@ -167,6 +177,10 @@ def main():
         peer_manager.connect_peer(
             config.get('network', 'remote_host'),
             config.getint('network', 'remote_port'))
+
+    if config.getboolean('misc', 'interactive'):
+        from IPython import embed
+        embed()
 
     # loop
     while not peer_manager.stopped():
